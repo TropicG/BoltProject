974caadb143470f26eb0d463ddafa1d92703f9f8
Konstantin Kondov <konstantin28102001@yahoo.com>
2025-03-24 16:55:34 +0200

    Adding Driver functionality

--- src/bg/sofia/fmi/uni/driver/AgeGroup.java
+++ src/bg/sofia/fmi/uni/driver/AgeGroup.java
@@ -1,4 +1,14 @@
 package bg.sofia.fmi.uni.driver;
 
 public enum AgeGroup {
+
+    JUNIOR(10),
+    EXPIRIENCED(0),
+    SENIOR(15);
+
+    private final int startingDeposit;
+
+    AgeGroup(final int startingDeposit) {
+        this.startingDeposit = startingDeposit;
+    }
 }
---
+++ src/bg/sofia/fmi/uni/driver/Driver.java
@@ -0,0 +1,4 @@
+package bg.sofia.fmi.uni.driver;
+
+public record Driver(AgeGroup ageGroup) {
+}
